1ee52c4008678d672cc63312d6ff97d87681ed7f
Juhnowski <juhnowski@gmail.com>
2018-07-10 02:58:30 +0300

    Initial Commit

---
+++ ObjC_YouTube/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjC_YouTube
+//
+//  Created by Илья Юхновский on 10.07.2018.
+//  Copyright © 2018 Илья Юхновский. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ObjC_YouTube/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ObjC_YouTube
+//
+//  Created by Илья Юхновский on 10.07.2018.
+//  Copyright © 2018 Илья Юхновский. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
